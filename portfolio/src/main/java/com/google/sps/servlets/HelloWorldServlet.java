010d61a1cb7a4f5093e801e09fa1aa2b3ce6fe6e
Robert <rvc6@duke.edu>
2021-03-10 17:52:27 +0000

    Comments section and hello button

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,18 +1,35 @@
 package com.google.sps.servlets;
-
+import java.util.ArrayList;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+    
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello world!");
-  }
+    ArrayList<String> stringList = new ArrayList<String>();
+    stringList.add("Hello world!");
+    stringList.add("Hey World!");
+    stringList.add("¡Hola Mundo!");
+    stringList.add("你好，世界！");
+    stringList.add("Bonjour le monde!");
+    
+
+    Gson gson = new Gson();
+    String json = gson.toJson(stringList);
+    
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+    
+    
+  }    
+ 
 }
