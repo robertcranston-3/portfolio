81095bc179830f3a7927b51e2e1f337af7c6fba2
Robert <rvc6@duke.edu>
2021-03-15 04:29:16 +0000

    added a map element that has to location markers with infoWindows

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -37,3 +37,55 @@ async function showGreeting(){
     const documentContainer = document.getElementById('hello');
     documentContainer.innerText=responseText[Math.floor(Math.random()*5)];
 }
+
+function initMap() {
+  let map;
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat:33.8550199 , lng:-84.2080488 },
+    zoom: 11,
+  });
+
+  const ABWString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading"'+">Atlanta's Best Wings</h1>" +
+    '<div id="bodyContent">' +
+    "<p><b>Atlanta's Best Wings</b>, also referred to as <b>ABW</b>,"+ 
+    "has the best wings I have ever had in my entire life.</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: ABWString,
+  });
+  const ABW = new google.maps.Marker({
+    position: {lat:33.8550199 , lng:-84.2080488 },
+    map,
+    title: "Atlanta's Best Wings",
+  });
+  ABW.addListener("click", () => {
+    infowindow.open(map, ABW);
+  });
+  const PhoString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">I Love Pho</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>I Love Pho</b>"+ 
+    "has delicious genuine vietnamese Pho.</p>" +
+    "</div>" +
+    "</div>";
+  const infowindowPho = new google.maps.InfoWindow({
+    content: PhoString,
+  });
+  const Pho = new google.maps.Marker({
+    position: {lat:33.9416915 , lng:-84.159816},
+    map,
+    title: "I love Pho",
+  });
+  Pho.addListener("click", () => {
+    infowindowPho.open(map, Pho);
+  });
+  
+}
